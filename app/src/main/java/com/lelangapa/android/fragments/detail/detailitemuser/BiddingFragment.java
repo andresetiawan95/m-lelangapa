0b171795c89edc2fe32b0883a548fa87caa0c4e9
Andre <setiawan.andre03@gmail.com>
2017-06-02 23:42:24 +0700

    [UPDATE] check session on BiddingFragment, ask not logged in user to login

--- app/src/main/java/com/lelangapa/android/fragments/detail/detailitemuser/BiddingFragment.java
+++ app/src/main/java/com/lelangapa/android/fragments/detail/detailitemuser/BiddingFragment.java
@@ -1,5 +1,6 @@
 package com.lelangapa.android.fragments.detail.detailitemuser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,12 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.lelangapa.android.R;
+import com.lelangapa.android.activities.LoginActivity;
 import com.lelangapa.android.interfaces.BidReceiver;
 import com.lelangapa.android.interfaces.DataReceiver;
+import com.lelangapa.android.preferences.SessionManager;
 import com.lelangapa.android.resources.DetailItemResources;
 import com.lelangapa.android.resources.NumberTextWatcher;
 
@@ -25,9 +29,10 @@ import java.text.DecimalFormat;
 public class BiddingFragment extends Fragment {
     private DetailItemResources detailItem;
     private TextView textView_namaBidder;
-    private TextView textView_hargaBid, textView_indicator;
+    private TextView textView_hargaBid, textView_askLogin;
     private EditText editText_inputPriceBid;
     private Button button_submitBid;
+    private LinearLayout linearLayout_biddingEnabled, linearLayout_biddingDisabled;
     private ProgressBar progressBar_indicator;
     private DataReceiver newBiddingDataReceived;
     private BidReceiver inputBidReceiver;
@@ -44,15 +49,7 @@ public class BiddingFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_detail_barang_bidding_submitbid_layout, container, false);
         initializeViews(view);
         displayBidInformation();
-        editText_inputPriceBid.addTextChangedListener(new NumberTextWatcher(editText_inputPriceBid));
-        button_submitBid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //belum dilakukan pengecekan inputan, langsung coba submit
-                String bidPriceFromUserInput = editText_inputPriceBid.getText().toString().trim().replaceAll("[^0-9]","");
-                inputBidReceiver.bidReceived(bidPriceFromUserInput);
-            }
-        });
+        validateUserSession();
         return view;
     }
     private void initializeConstant()
@@ -65,6 +62,9 @@ public class BiddingFragment extends Fragment {
         textView_hargaBid = (TextView) view.findViewById(R.id.fragment_detail_barang_bid_hargabid);
         editText_inputPriceBid = (EditText) view.findViewById(R.id.fragment_detail_barang_bid_pricetextbox);
         button_submitBid = (Button) view.findViewById(R.id.fragment_detail_barang_bid_submitbutton);
+        linearLayout_biddingEnabled = (LinearLayout) view.findViewById(R.id.fragment_detail_barang_bid_enabled);
+        linearLayout_biddingDisabled = (LinearLayout) view.findViewById(R.id.fragment_detail_barang_bid_disabled);
+        textView_askLogin = (TextView) view.findViewById(R.id.fragment_detail_barang_bid_disabled_ask_login);
         //progressBar_indicator = (ProgressBar) view.findViewById(R.id.fragment_detail_barang_bid_indicator_progress_bar);
         //textView_indicator = (TextView) view.findViewById(R.id.fragment_detail_barang_bid_indicator);
     }
@@ -72,6 +72,38 @@ public class BiddingFragment extends Fragment {
     {
         this.detailItem = detailItem;
         if (textView_namaBidder != null && textView_hargaBid != null) displayBidInformation();
+    }
+    private void validateUserSession() {
+        if (SessionManager.isLoggedInStatic()) {
+            linearLayout_biddingDisabled.setVisibility(View.GONE);
+            linearLayout_biddingEnabled.setVisibility(View.VISIBLE);
+            setupViewPropertiesWhenLoggedIn();
+        }
+        else {
+            linearLayout_biddingDisabled.setVisibility(View.VISIBLE);
+            linearLayout_biddingEnabled.setVisibility(View.GONE);
+            setupViewPropertiesWhenNotLoggedIn();
+        }
+    }
+    private void setupViewPropertiesWhenLoggedIn() {
+        editText_inputPriceBid.addTextChangedListener(new NumberTextWatcher(editText_inputPriceBid));
+        button_submitBid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //belum dilakukan pengecekan inputan, langsung coba submit
+                String bidPriceFromUserInput = editText_inputPriceBid.getText().toString().trim().replaceAll("[^0-9]","");
+                inputBidReceiver.bidReceived(bidPriceFromUserInput);
+            }
+        });
+    }
+    private void setupViewPropertiesWhenNotLoggedIn() {
+        textView_askLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     public void setInputReceiverToSubmitBidding(BidReceiver inputBidReceiver)
     {
